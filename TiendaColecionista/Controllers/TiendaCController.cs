55399738882020e52fde7900f3929830bcd642b9
Anto462 <0anthony0402@gmail.com>
2023-03-05 20:58:35 -0600

    Agregar archivos de proyecto.

---
+++ TiendaColecionista/Controllers/TiendaCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaColecionista.Controllers
+{
+    public class TiendaCController : Controller
+    {
+        // GET: TiendaC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TiendaColecionista/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TiendaColecionista.MvcApplication" Language="C#" %>
